d70fced88da6e23dfcd7491a7999691bba5c30d5
Tiago Tiede <geschopf@gmail.com>
2020-02-08 22:58:06 -0300

    [WiP] result API external

--- src/main/java/br/com/hurb/challengebravo/config/DataLoader.java
+++ src/main/java/br/com/hurb/challengebravo/config/DataLoader.java
@@ -1,11 +1,14 @@
 package br.com.hurb.challengebravo.config;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 @Component
 public class DataLoader implements ApplicationRunner {
@@ -15,9 +18,14 @@ public class DataLoader implements ApplicationRunner {
         RestTemplate restTemplate = new RestTemplate();
         String fooResourceUrl
             = "https://min-api.cryptocompare.com/data/";
-        ResponseEntity<String> response
-            = restTemplate.getForEntity(fooResourceUrl + "/price?fsym=USD&tsyms=BRL,EUR,BTC,ETH,USD", String.class);
-        System.out.println(response.getBody());
+        ResponseEntity<MyObject> response
+            = restTemplate.getForEntity(fooResourceUrl + "/price?fsym=USD&tsyms=BRL,EUR,BTC,ETH,USD", MyObject.class);
+        System.out.println(response);
 
     }
+}
+
+class MyObject  {
+    @JsonProperty(value="response")
+    private Map<String,String> response;
 }
